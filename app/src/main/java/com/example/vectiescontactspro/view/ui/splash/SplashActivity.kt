06efee20134f5af9fc6a88dd8d2cc366be25cb47
archunandev <arjunyadav8836@gmail.com>
2021-08-15 21:34:28 +0530

    update binding

--- app/src/main/java/com/example/vectiescontactspro/view/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/example/vectiescontactspro/view/ui/splash/SplashActivity.kt
@@ -2,7 +2,12 @@ package com.example.vectiescontactspro.view.ui.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.WindowManager
 import com.example.vectiescontactspro.databinding.ActivitySplashBinding
+import com.example.vectiescontactspro.view.ui.onbord.OnboardActivity
+import com.example.vectiescontactspro.view.utils.Utils
+import com.example.vectiescontactspro.view.utils.Utils.Companion.openActivity
 
 class SplashActivity : AppCompatActivity() {
 
@@ -12,5 +17,21 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        init()
+    }
+
+    private fun init() {
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler().postDelayed(
+            {
+                openActivity(OnboardActivity::class.java, this) {}
+
+            }, 2000
+        )
     }
 }
--- app/src/main/java/com/example/vectiescontactspro/view/utils/Utils.kt
+++ app/src/main/java/com/example/vectiescontactspro/view/utils/Utils.kt
@@ -1,2 +1,30 @@
 package com.example.vectiescontactspro.view.utils
 
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.ActivityOptions
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.os.Environment
+
+
+class Utils{
+
+    companion object{
+
+        fun <T> Context.openActivity(it: Class<T>, activity: Activity, extras: Bundle.() -> Unit = {}) {
+            val intent = Intent(this, it)
+            intent.putExtras(Bundle().apply(extras))
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                startActivity(intent, ActivityOptions.makeSceneTransitionAnimation(activity).toBundle())
+            }
+            /*activity.overridePendingTransition(R.anim.enter_right_to_left, R.anim.exit_left_to_right)*/
+        }
+
+    }
+
+}
